ab0e0bce9ee4fc138e1acb6106cef21614968c3f
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2023-09-29 23:40:56 +0000

    docs: temporarily remove info from AboutUs section
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- src/routes/components/AboutUs.svelte
+++ src/routes/components/AboutUs.svelte
@@ -1,26 +1,8 @@
 <!-- TeamInfo.svelte -->
 <section id="team" class="team">
     <div class="team-content">
-      <h2>About the Team</h2>
-      <p>
-        Where Brilliance Meets Dedication 🌟
-      </p>
-  
-      <ul>
-        <li><strong>Visionaries at Heart:</strong> At the helm of BR Working Group, you'll find visionaries who are passionate about driving innovation. Our leadership team is composed of forward-thinkers who see the bigger picture and set the course for our collective success.</li>
-        <li><strong>Diverse Expertise:</strong>Our team is a mosaic of diverse talents, from seasoned developers and tech gurus to creative thinkers and business minds. This rich blend of expertise ensures that we approach challenges from multiple angles, finding the most effective solutions.</li>
-        <li><strong>Uniting Passion:</strong>We're not just colleagues; we're a close-knit family bound by our shared passion for innovation. Our members are driven by a common purpose—to see Brazil become a global innovation powerhouse—and this commitment fuels our everyday efforts.</li>
-        <li><strong>Collaborative Spirit:</strong>Collaboration is in our DNA. We thrive on open communication, idea sharing, and constructive feedback. Together, we harness the power of teamwork to turn concepts into reality, always pushing the boundaries of what's possible.</li>
-        <li><strong>Lifelong Learners:</strong>In the ever-evolving landscape of technology and innovation, our team members are perpetual learners. We embrace continuous growth, keeping our skills and knowledge at the cutting edge of the industry.</li>
-     </ul>
-  
-      <p>
-        Our team is not just about working together; it's about <strong>creating a movement that drives Brazil's innovation</strong> agenda. With <strong>passion, expertise, and unwavering</strong> commitment, the BR Working Group Team is paving the way for a brighter future — one where <strong>innovation knows no bounds and dreams become reality.</strong>
-      </p>
-  
-      <p>
-        Join us on this exciting journey, and together, we'll shape the future of innovation in Brazil and beyond. Welcome to the 🇧🇷 <strong>BR Working Group Team, where brilliance meets dedication.</strong>
-      </p>
+      <!-- <h2>About the Team</h2>
+      <p></p> -->
     </div>
   </section>
   
